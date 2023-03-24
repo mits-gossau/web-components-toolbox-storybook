44be0666c145b8cc262c3c2cd8a8d718dde41384
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-03-24 15:30:21 +0100

    style(Teaser.stories.js): format code and add line breaks for better readability feat(Teaser.stories.js): add line breaks and indentation to the a-link component in Download section for better readability
    feat(Teaser.stories.js): add Overlay and OverlayTop stories to Teaser molecule

--- src/stories/molecules/Teaser.stories.js
+++ src/stories/molecules/Teaser.stories.js
@@ -49,7 +49,10 @@ export const Download = () =>
             <h5>Broschüre Gemeinschaftsgastronomie</h5>
             <p>der Genossenschaft Migros Luzern</p>
             <p>
-              <a-link namespace="download-" icon-path="src/web-components-toolbox/src/es/components/molecules/teaser/download-/img/download.svg">
+              <a-link
+                namespace="download-"
+                icon-path="src/web-components-toolbox/src/es/components/molecules/teaser/download-/img/download.svg"
+              >
                 <a
                   href="https://www.betriebsrestaurants-migros.ch/dam/jcr:4462224a-82f5-41bb-a940-3a63f0e8bed6/Dokument-Gemeinschaftsgastronomie.pdf"
                   >PDF <span>(1.1 MB)</span></a
@@ -72,7 +75,10 @@ export const Download = () =>
             <h5>Broschüre Gemeinschaftsgastronomie</h5>
             <p>der Genossenschaft Ostschweiz</p>
             <p>
-              <a-link namespace="download-" icon-path="src/web-components-toolbox/src/es/components/molecules/teaser/download-/img/download.svg">
+              <a-link
+                namespace="download-"
+                icon-path="src/web-components-toolbox/src/es/components/molecules/teaser/download-/img/download.svg"
+              >
                 <a
                   href="https://www.betriebsrestaurants-migros.ch/dam/jcr:4462224a-82f5-41bb-a940-3a63f0e8bed6/Dokument-Gemeinschaftsgastronomie.pdf"
                   >Ein PDF <span>(1.1 MB)</span></a
@@ -84,3 +90,104 @@ export const Download = () =>
       </m-teaser>
     </o-wrapper>
   `;
+
+export const Overlay = () => html`
+  <o-wrapper namespace="wrapper-teaser-">
+    <m-teaser namespace="teaser-overlay-" href="https://www.migrosbank.ch">
+      <figure>
+        <a-picture
+          namespace="picture-teaser-"
+          picture-load
+          defaultSource="src/web-components-toolbox/src/es/components/molecules/teaser/overlay-/img/unsere_werte_2.jpg"
+          alt="randomized image"
+        ></a-picture>
+        <figcaption>
+          <h2 class="bg-color">Unsere Werte</h2>
+          <h2 class="bg-color"><a-arrow move direction="right"></a-arrow></h2>
+        </figcaption>
+      </figure>
+    </m-teaser>
+    <m-teaser
+      namespace="teaser-overlay-"
+      href="https://www.stadlerrail.com/de/ueber-uns/standorte/stadler-bussnang-ag/"
+    >
+      <figure>
+        <a-picture
+          namespace="picture-teaser-"
+          picture-load
+          defaultSource="src/web-components-toolbox/src/es/components/molecules/teaser/overlay-/img/angebote-und-services_buehne2_1920x1080px.jpg"
+          alt="randomized image"
+        ></a-picture>
+        <figcaption>
+          <h2 class="bg-color">Unsere Angebote & Services</h2>
+          <h2 class="bg-color"><a-arrow move direction="right"></a-arrow></h2>
+        </figcaption>
+      </figure>
+    </m-teaser>
+  </o-wrapper>
+  <o-wrapper namespace="wrapper-teaser-" simulate-children="2">
+    <style>
+      :host {
+        --teaser-overlay-bg-background-color-hover-custom: #003a56;
+        --teaser-overlay-bg-background-color-custom: #26b4e9;
+      }
+    </style>
+    <m-teaser namespace="teaser-overlay-" href="http://www.jowa.ch">
+      <figure>
+        <a-picture
+          namespace="picture-teaser-"
+          picture-load
+          defaultSource="src/web-components-toolbox/src/es/components/molecules/teaser/overlay-/img/MV_Brunchschiff_Text_Banner_700x540px_sRGB_high.jpg"
+          alt="randomized image"
+        ></a-picture>
+        <figcaption>
+          <h2 class="bg-color"><a-arrow move direction="right"></a-arrow></h2>
+        </figcaption>
+      </figure>
+    </m-teaser>
+  </o-wrapper>
+  <m-teaser namespace="teaser-overlay-" href="http://www.jowa.ch">
+    <figure>
+      <a-picture
+        namespace="picture-teaser-"
+        picture-load
+        defaultSource="src/web-components-toolbox/src/es/components/molecules/teaser/overlay-/img/MV_Brunchschiff_Text_Banner_700x540px_sRGB_high.jpg"
+        alt="randomized image"
+      ></a-picture>
+      <figcaption>
+        <h2 class="bg-color"><a-arrow move direction="right"></a-arrow></h2>
+      </figcaption>
+    </figure>
+  </m-teaser>
+`;
+
+export const OverlayTop = () => html`
+  <o-wrapper namespace="wrapper-teaser-recipe-">
+    <m-teaser namespace="teaser-overlay-top-" href="">
+      <figure>
+        <a-picture
+          namespace="picture-"
+          alt="Nature Tisane Bouteille Naturopathie Resized"
+          defaultSource="src/web-components-toolbox/src/es/components/molecules/teaser/overlay-top-/img/nature-tisane-bouteille-naturopathie_resized.webp"
+          aspect-ratio="1"
+        ></a-picture>
+        <figcaption>
+          <h2 class="bg-color">Krasser Titel</h2>
+        </figcaption>
+      </figure>
+    </m-teaser>
+    <m-teaser namespace="teaser-overlay-top-" href="">
+      <figure>
+        <a-picture
+          namespace="picture-"
+          alt="Nature Tisane Bouteille Naturopathie Resized"
+          defaultSource="src/web-components-toolbox/src/es/components/molecules/teaser/overlay-top-/img/nature-tisane-bouteille-naturopathie_resized.webp"
+          aspect-ratio="1"
+        ></a-picture>
+        <figcaption>
+          <h2 class="bg-color">Krasser Titel</h2>
+        </figcaption>
+      </figure>
+    </m-teaser>
+  </o-wrapper>
+`;
