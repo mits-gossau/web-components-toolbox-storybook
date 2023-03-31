f915f107ec8c4e85bfc6546a5c49c6e0ce2bf8a5
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-03-31 11:35:41 +0200

    feat(storybook): build static files for Storybook

---
+++ storybook-static/src/mandants/web-components-toolbox-mdx/README.md
@@ -0,0 +1,5 @@
+# web-components-toolbox-mdx
+
+The web component toolbox to be used with [MDX](https://mdxjs.com/).
+
+WIP
---
+++ storybook-static/src/stories/atoms/IconPaperclip.stories.js
@@ -0,0 +1,13 @@
+// IconPaperclip.stories.js
+import { html } from "lit-html";
+
+import("../../web-components-toolbox/src/es/components/atoms/iconPaperclip/IconPaperclip").then(
+  (module) => self.customElements.define("a-icon-paperclip", module.default)
+);
+
+export default {
+  title: "Atoms/IconPaperclip",
+  component: "a-icon-paperclip",
+};
+
+export const Default = () => html` <a-icon-paperclip /> `;
