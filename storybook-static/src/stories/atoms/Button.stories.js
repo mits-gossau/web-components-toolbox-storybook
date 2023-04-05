869eb1c393ea2d8484deded107812157c5eb5ed6
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-04-05 11:58:35 +0200

    feat(storybook): build static files for Storybook

--- storybook-static/src/stories/atoms/Button.stories.js
+++ storybook-static/src/stories/atoms/Button.stories.js
@@ -10,17 +10,33 @@ export default {
   component: "a-button",
   argTypes: {
     disabled: {
-      control: 'boolean',
+      control: "boolean",
     },
     label: {
-      control: 'text',
-    }
+      control: "text",
+    },
   },
   args: {
     disabled: false,
-    label: 'Button',
-  }
+    label: "Button",
+  },
 };
+
+// export const Primary = {
+//   render: (args) => {
+//     const btn = document.createElement('a-button');
+
+//     btn.setAttribute("namespace",'button-primary-')
+//     btn.innerText = args.label ? args.label : 'Button';
+//     args.disabled ? btn.setAttribute('disabled', 'disabled') : undefined;
+    
+//     return btn;
+//   },
+//   args: {
+//     disabled: true,
+//     label: 'Button',
+//   },
+// };
 
 export const Primary = () => html`
   <a-button namespace="button-primary-">Button</a-button>
