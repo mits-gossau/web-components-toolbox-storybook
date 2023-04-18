d562621f1f9b16f33adf58488448a18721469c0d
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-04-18 11:58:50 +0200

    feat(storybook): build static files for Storybook

---
+++ storybook-static/sb-addons/storybook-7/manager-bundle.mjs
@@ -0,0 +1,4 @@
+try{
+var d=__STORYBOOKADDONS__,{addons:r,types:_,mockChannel:b}=__STORYBOOKADDONS__;var f=__STORYBOOKTHEMING__,{CacheProvider:u,ClassNames:x,Global:C,ThemeProvider:S,background:y,color:k,convert:N,create:s,createCache:R,createGlobal:w,createReset:B,css:D,darken:G,ensure:K,ignoreSsrWarning:Y,isPropValid:v,jsx:I,keyframes:P,lighten:A,styled:E,themes:H,typography:M,useTheme:W,withTheme:j}=__STORYBOOKTHEMING__;var a=s({base:"light",brandTitle:"Web Components",brandUrl:"https://github.com/mits-gossau/web-components-toolbox-storybook",brandImage:"src/assets/images/web-components-350x150.png",brandTarget:"_self"});r.setConfig({theme:a});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.mjs.map
