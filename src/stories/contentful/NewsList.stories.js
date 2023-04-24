d248a20dfb5433572938a0be981f3443f71c29b0
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-04-24 16:38:39 +0200

    refactor(NewsList.stories.js): remove unnecessary absolute paths in news-url attribute of contentful-news-list component

--- src/stories/contentful/NewsList.stories.js
+++ src/stories/contentful/NewsList.stories.js
@@ -14,22 +14,10 @@ export default {
 
 export const Default = () =>
   html`<contentful-news-list
-      answer-event-name="list-news"
-      request-event-name="request-list-news"
-      namespace="news-list-default-"
-      news-url="/src/web-components-toolbox/docs/Template.html?logo=/src/web-components-toolbox/src/es/components/atoms/logo/default-/default-.html&nav=/src/web-components-toolbox/src/es/components/molecules/navigation/default-/default-.html&footer=/src/web-components-toolbox/src/es/components/organisms/footer/default-/default-.html&content=/src/web-components-toolbox/src/es/components/molecules/news/default-/default-.html"
-      news-preview-namespace=""
-    >
-    </contentful-news-list>
-    <script
-      type="text/javascript"
-      src="/src/web-components-toolbox/wc-config.js?triggerImmediately=true"
-    ></script>
-    <script
-      type="text/javascript"
-      src="/src/web-components-toolbox/src/es/helpers/Environment.js"
-    ></script>
-    <script
-      type="text/javascript"
-      src="/src/web-components-toolbox/docs/es/documenter.js?component=NewsList.js"
-    ></script> `;
+    answer-event-name="list-news"
+    request-event-name="request-list-news"
+    namespace="news-list-default-"
+    news-url="src/web-components-toolbox/docs/Template.html?logo=src/web-components-toolbox/src/es/components/atoms/logo/default-/default-.html&nav=src/web-components-toolbox/src/es/components/molecules/navigation/default-/default-.html&footer=src/web-components-toolbox/src/es/components/organisms/footer/default-/default-.html&content=src/web-components-toolbox/src/es/components/molecules/news/default-/default-.html"
+    news-preview-namespace=""
+  >
+  </contentful-news-list> `;
