068fa8bc8dfb73fb58dca902c18fd1e4fbaa33a7
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-05-05 14:10:22 +0200

    feat(Arrow.stories.js): add more examples to Default story chore(web-components-toolbox): update submodule to commit f181461c17e5818e71a537bb2d3d88789c832b18

--- src/stories/atoms/Arrow.stories.js
+++ src/stories/atoms/Arrow.stories.js
@@ -10,4 +10,16 @@ export default {
   component: "a-arrow",
 };
 
-export const Default = () => html` <a-arrow /> `;
+export const Default = () =>
+  html` <div>
+    <p><a-arrow /></p>
+    <hr />
+    <p><a-arrow direction="up" /></p>
+    <p><a-arrow direction="right" /></p>
+    <p><a-arrow direction="down" /></p>
+    <p><a-arrow direction="left" /></p>
+    <hr />
+    <p><a-arrow move direction="down" /></p>
+    <p><a-arrow move hover-on-parent-shadow-root-host direction="right" /></p>
+    <p><a-arrow move hover-on-parent-shadow-root-host direction="down" /></p>
+  </div>`;
