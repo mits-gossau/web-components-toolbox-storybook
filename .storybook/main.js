bf14a3b6cc2a3848f23f3f4db5655e7922018966
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-06-07 13:22:17 +0200

    style(main.js): fix indentation and quotes chore(main.js): remove unnecessary 'autodocs' option from docs configuration
    style(preview.js): reformat code and remove unnecessary commas
    
    style(preview.js): remove trailing comma in the parameters object
    
    chore(package.json): add @storybook/blocks dependency
    chore(web-components-toolbox-steps): update subproject commit to a29531c5d8e687381a9eb58585d1623ca546660b
    
    feat(introduction.mdx): add introduction page with list of supported projects and theme toggle button

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,22 @@
 module.exports = {
   staticDirs: [{
-    from: "../src",
-    to: "/src"
+    from: '../src',
+    to: '/src'
   }],
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@etchteam/storybook-addon-css-variables-theme", "@storybook/addon-links", {
-    name: "@storybook/addon-essentials",
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@etchteam/storybook-addon-css-variables-theme', '@storybook/addon-links', {
+    name: '@storybook/addon-essentials',
     options: {
       backgrounds: false,
       measure: false,
       outline: false
     }
-  }, 'storybook-addon-themes', "@storybook/addon-mdx-gfm"],
+  }, 'storybook-addon-themes', '@storybook/addon-mdx-gfm'],
   framework: {
-    name: "@storybook/web-components-webpack5",
+    name: '@storybook/web-components-webpack5',
     options: {}
   },
   docs: {
-    autodocs: true
+    autodocs: false
   }
-};
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,78 +1,76 @@
 // .storybook/preview.js
-import cssVariablesTheme from '@etchteam/storybook-addon-css-variables-theme';
+import cssVariablesTheme from '@etchteam/storybook-addon-css-variables-theme'
 
-import "../src/web-components-toolbox/src/css/initial.css";
-import "../src/web-components-toolbox/src/css/reset.css";
-import "../src/web-components-toolbox/src/css/colors.css";
-import "../src/web-components-toolbox/src/css/fonts.css";
-import "../src/web-components-toolbox/src/css/variables.css";
+import '../src/web-components-toolbox/src/css/initial.css'
+import '../src/web-components-toolbox/src/css/reset.css'
+import '../src/web-components-toolbox/src/css/colors.css'
+import '../src/web-components-toolbox/src/css/fonts.css'
+import '../src/web-components-toolbox/src/css/variables.css'
 
-import themeAlnatura from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-alnatura/src/css/variablesCustom.css';
-import themeBetriebsrestaurants from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-betriebsrestaurant/src/css/variablesCustom.css';
-import themeBuelachSued from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-buelach-sued/src/css/variablesCustom.css';
-import themeEatery from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-eatery/src/css/variablesCustom.css';
-import themeHitzberger from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-hitzberger/src/css/variablesCustom.css';
-import themeKaimug from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-kaimug/src/css/variablesCustom.css';
-import themeKlubschule from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-klubschule/src/css/variablesCustom.css';
-import themeLogistikplattform from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-logistikplattform/src/css/variablesCustom.css';
-import themeMakerSpace from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-maker-space/src/css/variablesCustom.css';
-import themeMDX from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-mdx/src/css/variablesCustom.css';
-import themeMigrosCity from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-migros-city/src/css/variablesCustom.css';
-import themeMigrosPro from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-migrospro/src/css/variablesCustom.css';
-import themeMitreva from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-mitreva/src/css/variablesCustom.css';
-import themeNature from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-nature/src/css/variablesCustom.css';
-import themeNeumarktAltstetten from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-neumarkt-altstetten/src/css/variablesCustom.css';
-import themeOrangerGarten from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-orangergarten/src/css/variablesCustom.css';
-import themeParkImGruene from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-parkimgruene/src/css/variablesCustom.css';
-import themeStageOne from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-stage-one/src/css/variablesCustom.css';
-import themeSteps from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-steps/src/css/variablesCustom.css';
-import themeTechDay from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-techday/src/css/variablesCustom.css';
-import themeYearbooksIndustrie from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-yearbooks/src/css/variablesIndustrie.css';
-import themeYearbooksMiduca from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-yearbooks/src/css/variablesMiduca.css';
-import themeYearbooksOperations from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-yearbooks/src/css/variablesOperations.css';
-import themeZueriseeCenter from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-zueriseecenter/src/css/variablesCustom.css';
-
-
+import themeAlnatura from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-alnatura/src/css/variablesCustom.css'
+import themeBetriebsrestaurants from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-betriebsrestaurant/src/css/variablesCustom.css'
+import themeBuelachSued from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-buelach-sued/src/css/variablesCustom.css'
+import themeEatery from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-eatery/src/css/variablesCustom.css'
+import themeHitzberger from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-hitzberger/src/css/variablesCustom.css'
+import themeKaimug from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-kaimug/src/css/variablesCustom.css'
+import themeKlubschule from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-klubschule/src/css/variablesCustom.css'
+import themeLogistikplattform from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-logistikplattform/src/css/variablesCustom.css'
+import themeMakerSpace from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-maker-space/src/css/variablesCustom.css'
+import themeMDX from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-mdx/src/css/variablesCustom.css'
+import themeMigrosCity from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-migros-city/src/css/variablesCustom.css'
+import themeMigrosPro from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-migrospro/src/css/variablesCustom.css'
+import themeMitreva from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-mitreva/src/css/variablesCustom.css'
+import themeNature from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-nature/src/css/variablesCustom.css'
+import themeNeumarktAltstetten from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-neumarkt-altstetten/src/css/variablesCustom.css'
+import themeOrangerGarten from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-orangergarten/src/css/variablesCustom.css'
+import themeParkImGruene from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-parkimgruene/src/css/variablesCustom.css'
+import themeStageOne from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-stage-one/src/css/variablesCustom.css'
+import themeSteps from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-steps/src/css/variablesCustom.css'
+import themeTechDay from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-techday/src/css/variablesCustom.css'
+import themeYearbooksIndustrie from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-yearbooks/src/css/variablesIndustrie.css'
+import themeYearbooksMiduca from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-yearbooks/src/css/variablesMiduca.css'
+import themeYearbooksOperations from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-yearbooks/src/css/variablesOperations.css'
+import themeZueriseeCenter from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/clients/web-components-toolbox-zueriseecenter/src/css/variablesCustom.css'
 
 export const decorators = [
-  cssVariablesTheme,
-];
+  cssVariablesTheme
+]
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
   cssVariables: {
     files: {
       'No Theme': '',
-      'Alnatura': themeAlnatura,
-      'Betriebsrestaurants': themeBetriebsrestaurants,
+      Alnatura: themeAlnatura,
+      Betriebsrestaurants: themeBetriebsrestaurants,
       'Bülach Süd': themeBuelachSued,
-      'Eatery': themeEatery,
-      'Hitzberger': themeHitzberger,
-      'Kaimug': themeKaimug,
-      'Klubschule': themeKlubschule,
-      'Logistikplattform': themeLogistikplattform,
+      Eatery: themeEatery,
+      Hitzberger: themeHitzberger,
+      Kaimug: themeKaimug,
+      Klubschule: themeKlubschule,
+      Logistikplattform: themeLogistikplattform,
       'Maker Space': themeMakerSpace,
-      'MDX': themeMDX,
+      MDX: themeMDX,
       'Migros City': themeMigrosCity,
-      'MigrosPro': themeMigrosPro,
-      'Mitreva': themeMitreva,
-      'Nature': themeNature,
+      MigrosPro: themeMigrosPro,
+      Mitreva: themeMitreva,
+      Nature: themeNature,
       'Neumarkt Altstetten': themeNeumarktAltstetten,
       'Oranger Garten': themeOrangerGarten,
       'Park im Grüene': themeParkImGruene,
-      'StageOne': themeStageOne,
-      'Steps': themeSteps,
-      'TechDay': themeTechDay,
+      StageOne: themeStageOne,
+      Steps: themeSteps,
+      TechDay: themeTechDay,
       'Yearbooks Industrie': themeYearbooksIndustrie,
       'Yearbooks Miduca': themeYearbooksMiduca,
       'Yearbooks Operations': themeYearbooksOperations,
-      'Zürisee Center': themeZueriseeCenter,
+      'Zürisee Center': themeZueriseeCenter
     },
     defaultTheme: 'No Theme'
   },
@@ -82,6 +80,6 @@ export const parameters = {
       { name: 'm', class: 'themeMLight', color: 'rgb(255, 102, 0)' },
       { name: 'showcase', class: 'themeShowcaseLight', color: 'rgb(255, 191, 41)' }
     ],
-    target: 'root',
-  },
+    target: 'root'
+  }
 }
